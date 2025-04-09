1b2f290e725e3f647a29e29a53d93f1e02cae28c
Faizan <131034033+faizanparwez@users.noreply.github.com>
2025-04-10 01:44:53 +0530

    succesfully integrated user authentication

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // To accept JSON body
 
+
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
 // Test route
 app.get("/", (req, res) => {
   res.send("API is working!");
